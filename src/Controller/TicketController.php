ce29a2ac268bf9cf5471a0a74573febefed1c882
Geoffroy Cochard <geoffroycochard@gmail.com>
2023-01-23 14:07:10 +0000

    CRUD lit / edit

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ticket;
+use App\Form\TicketType;
+use App\Repository\TicketRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketController extends AbstractController
+{
+    #[Route('/ticket', name: 'app_ticket')]
+    public function index(TicketRepository $ticketRepository): Response
+    {
+        return $this->render('ticket/index.html.twig', [
+            'tickets' => $ticketRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/ticket/{id}', name: 'app_ticket_edit')]
+    public function edit(
+        Ticket $ticket, 
+        Request $request,
+        TicketRepository $ticketRepository
+    )
+    {
+        $form = $this->createForm(
+            TicketType::class,
+            $ticket
+        );
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ticket = $form->getData();
+            $ticketRepository->save($ticket, true);
+            return $this->redirectToRoute('app_ticket');
+        }
+
+        return $this->render('ticket/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
---
+++ src/Form/TicketType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('date')
+            ->add('category', null, [
+                'choice_label' => 'title'
+            ])
+            ->add('users', null, [
+                'choice_label' => 'username'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
